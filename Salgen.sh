f745f6a2334959d9df0c4b7ae3ae60167d778638
zlcfdu17 <lczhou17@fudan.edu.cn>
2019-06-02 12:57:21 +0800

    fix bugs

--- Salgen.sh
+++ Salgen.sh
@@ -1,10 +1,10 @@
 
-rm -rf ./Tpmapgen/tmp
+rm -f ./Tpmapgen/tmp
 mkdir ./Tpmapgen/tmp
 
 matlab -nodesktop -nosplash -r Tpmapgen.m
 
 python Salpredict.py
 
-rm -rf ./Tpmapgen/tmp
+rm -f ./Tpmapgen/tmp
 
